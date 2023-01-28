9fe9b59d02a9edf9ab11cc7ca29916c7f20dfadc
Pontus <ponta.berglund.pb@gmail.com>
2023-01-29 00:06:59 +0100

    Create Main.java. Add a Calculator constructor.

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+import java.awt.*;
+import java.beans.Expression;
+
+public class Main {
+    public static void main(String[] args) {
+        Calculator cal = new Calculator(500, 800);
+    }
+}
+
+//TODO: Style buttons and make the font size dynamic
+//TODO: Add support for parenthesis
